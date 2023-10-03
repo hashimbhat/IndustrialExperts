b9a37b71ea5c0ad758450756cf74227bbaea6488
Hashim <93281694+hashimbhat@users.noreply.github.com>
2023-10-03 13:38:33 +0530

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Industrial_Experts
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        protected void empbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cusbtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CustomerSingup.aspx");
+        }
+
+        protected void empbtn_Click1(object sender, EventArgs e)
+        {
+            Response.Redirect("EmployeeSignup.aspx");
+
+        }
+
+        protected void conbtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ContractorSignup.aspx");
+        }
+    }
+}
